b1a4dfe07aadc0a23f2eb699beb636ca3d464767
Craig <craig.coleman.cc@gmail.com>
2021-12-13 09:19:54 -0600

    commit tree 77a58de213f4f54f6c00773d0707b539819596a1

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let reverseWord = word.split('').reverse().join('');
+  return reverseWord === word ? true : false;
 }
 
 /* 
-  Add your pseudocode here
+  Use if/else to see if argument is equal to the reverse order of the argument
+  if equal return true
+  if not equal (else) return false
 */
 
 /*
-  Add written explanation of your solution here
+  .split('') is applied to the argument, splitting all of it characters into an array
+  .reverse() is then applied, reversing the order of the characters in the array
+  .join('') is then applied to put the characters back together in a string
+  this new string (assigned to the variable reverseWord) is then tested against the argument with an if/then
+  the ternary operator is used to check if word and reverseWord are equal
+  if they are, return true, if not, return false
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("gohangasalamiimalasagnahog"))
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("endoplasmicreticulum"))
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome('mom'));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome('dexoyribose'))
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
